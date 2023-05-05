2403a374fe1f1dadb65732d0782fbb20db6bbffc
owermaiden <ower.maiden@cybertekschool.com>
2023-05-05 16:07:21 -0400

    type

--- src/app/typescript/typescript/typescript.component.ts
+++ src/app/typescript/typescript/typescript.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-typescript',
@@ -6,6 +8,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./typescript.component.css']
 })
 export class TypescriptComponent {
+
+  constructor(@Inject(DOCUMENT) private document: Document){}
 
   // Variable types
   aString : string = 'This is just a string';
@@ -31,6 +35,20 @@ export class TypescriptComponent {
 
   }
 
+  // String Methods are very close to Java 
+  stringMethods(givenString: string){
+
+    givenString.toLowerCase();
+    givenString.split(',');
+    givenString.trim();
+
+    let concatination: string = `${this.aString} works in the ${this.aNumber} department.`; 
+    concatination.charAt(0);
+    concatination.length;
+    
+  }
+
+  // Array => There is no other data structure like array & arraylist... Just Array
   arrayMethods(givenArray: any[]){
 
     givenArray.push(10);
@@ -43,5 +61,80 @@ export class TypescriptComponent {
      
   }
 
+  // Loops
+  loopsInTypescript(){
+
+    // for Loop
+    for (let i = 0; i < 3; i++) {
+      console.log ("Block statement execution no." + i);
+    }
+
+    // forEach Loop
+    let arr = [10, 20, 30, 40];
+    for (var val of arr) {
+      console.log(val); // prints values: 10, 20, 30, 40
+    }
+
+    // While Loop
+    let i: number = 2;
+    while (i < 4) {
+        console.log( "Block statement execution no." + i )
+        i++;
+    }
+
+    //do-while Loop
+    let x: number = 2;
+    do {
+        console.log("Block statement execution no." + x )
+        x++;
+    } while ( i < 4)
+
+
+  }
+
+  // If-Else Statement
+  ifElseTypeScript(){
+    let x: number = 10, y = 20;
+
+    if (x > y) {
+        console.log('x is greater than y.');
+    } else if (x < y){
+        console.log('x is less than y.'); 
+    } else if (x == y) {
+        console.log('x is equal to y');
+    }
+  }
+
+  // Switch-Case
+  switchCaseTypeScript(){
+    let x = 10, y = 5;
+    switch (x-y) {
+        case 0:
+            console.log("Result: 0");
+            break;
+        case 5:
+            console.log("Result: 5");
+            break;
+        case 10:
+            console.log("Result: 10");
+            break;
+    }
+  }
+
+  navigateToTypeScript(){
+    this.document.location.href = 'https://www.tutorialsteacher.com/typescript';
+  }
+
  
 }
+
+
+// class example
+class Employee {
+  public empCode?: string;
+  empName?: string; // public
+}
+
+let emp = new Employee();
+emp.empCode = '123';
+emp.empName = "Swati";
